4ed6e791a560bb5f29e915ba53e45c75ada9fd77
Sonjyoti Rabha <85861164+sonjyoti@users.noreply.github.com>
2024-01-05 12:42:55 +0530

    Commented Main.java

--- 5th-Sem/Java/Main.java
+++ 5th-Sem/Java/Main.java
@@ -1,13 +1,15 @@
+/*
 public class Main {
     public static void main(String[] args) {
-    /*
+        
         Question1 customer = new Question1(1500);
         customer.depositBal(500);
         customer.withdraw(15000);
         customer.displayDetails();
-    **/
+    
 
         Question8 teacher = new Question8();
         teacher.showData();
     }
 }
+**/
